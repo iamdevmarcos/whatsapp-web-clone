6afd99a7556f292d9342815b7de0225a49998e6b
Marcos Andre <marcosdev.me@gmail.com>
2021-12-17 15:16:46 -0300

    creating interface

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from '@material-ui/icons/Chat';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="appWindow">
+      <div className="sideBar">
+        <header>
+          <img className="headerAvatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
+
+          <div className="headerButtons">
+            <div className="headerBtn">
+              <DonutLargeIcon style={{color:'#919191'}} />
+            </div>
+            <div className="headerBtn">
+              <ChatIcon style={{color:'#919191'}} />
+            </div>
+            <div className="headerBtn">
+              <MoreVertIcon style={{color:'#919191'}} />
+            </div>
+          </div>
+        </header>
+        <div className="search">...</div>
+        <div className="chatList">...</div>
+      </div>
+      <div className="contentArea">...</div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

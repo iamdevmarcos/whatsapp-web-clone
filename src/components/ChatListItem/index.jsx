edd587bf2eba8380f5123b2c8c31248f86860ac7
Marcos Andre <marcosdev.me@gmail.com>
2021-12-17 21:10:58 -0300

    creating interface of chatlist

--- src/components/ChatListItem/index.jsx
+++ src/components/ChatListItem/index.jsx
@@ -2,8 +2,20 @@ import './style.css';
 
 export const ChatListItem = () => {
     return (
-        <div>
-            ChatList....
+        <div className='chatListItem'>
+            <img className="chatListItemAvatar" src="https://www.w3schools.com/howto/img_avatar2.png" alt="" />
+
+            <div className="chatListItemLines">
+                <div className="chatListItemLine">
+                    <div className="chatListItemName">Marcos Andre</div>
+                    <div className="chatListItemDate">20:00</div>
+                </div>
+                <div className="chatListItemLine">
+                    <div className="chatListItemLastMessage">
+                        <p>Opa, tudo bem ?</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }

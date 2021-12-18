ec33799a89c2cc4b5e8158db45dd31d69230ae74
Marcos Andre <marcosdev.me@gmail.com>
2021-12-18 13:52:47 -0300

    ajusting stead

--- src/components/ChatIntro/index.jsx
+++ src/components/ChatIntro/index.jsx
@@ -3,7 +3,7 @@ import './style.css';
 export const ChatIntro = () => {
     return (
         <div className="chatIntro">
-            <img src="/public/assets/images/chatIntro.jpg" alt=""/>
+            <img src="/assets/images/chatIntro.jpg" alt=""/>
             <h1>Keep your cell phone connected.</h1>
             <h2>Whatsapp connects to your phone to sync your messages. To reduce data usage, connect your phone to a Wi-Fi network.</h2>
         </div>

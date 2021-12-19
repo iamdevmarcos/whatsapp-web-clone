54b35cfa191091f797b702dc8ed45e8243e02727
Marcos Andre <marcosdev.me@gmail.com>
2021-12-19 16:14:54 -0300

    Starting firebase connection

---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBcopV9q98jP6kA4jnuN42CKcJB5xjV5SU",
+    authDomain: "whatsappwebclone-694b7.firebaseapp.com",
+    projectId: "whatsappwebclone-694b7",
+    storageBucket: "whatsappwebclone-694b7.appspot.com",
+    messagingSenderId: "813831837485",
+    appId: "1:813831837485:web:3a639269a621d4f1eee5a4"
+};
+
+export default firebaseConfig;
